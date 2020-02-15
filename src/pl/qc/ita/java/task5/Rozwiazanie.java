4de465ec85fa160163f085174de610e3d120f17e
Iwona Kulesza <tsubame1990@gmail.com>
2020-02-15 12:04:16 +0100

    task 5

--- src/pl/qc/ita/java/task5/Rozwiazanie.java
+++ src/pl/qc/ita/java/task5/Rozwiazanie.java
@@ -7,8 +7,20 @@ Wynik dla 50111:
 Parzyste: 1 Nieparzyste: 4
  */
 
+import java.util.Scanner;
+
 public class Rozwiazanie {
     public static void main(String[] args) {
-        //Twój kod
+        Scanner scanner = new Scanner(System.in);
+        int parzyste = 0;
+        int nieparzyste = 0;
+        String[]input = scanner.next().split("");
+        for (String znak:input
+             ) {
+            if (Integer.parseInt(znak) % 2 == 0){
+                parzyste++;
+            }else nieparzyste++;
+        }
+        System.out.println("Parzyste: "+parzyste+" Nieparzyste: "+nieparzyste);
     }
 }
