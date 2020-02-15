06fa0b52c299362279172ab5e358aa53b2581425
kapogon <kpogonowski@o2.pl>
2020-02-15 11:50:34 +0100

    zad7 java

--- src/pl/qc/ita/java/task7/Rozwiazanie.java
+++ src/pl/qc/ita/java/task7/Rozwiazanie.java
@@ -6,8 +6,24 @@ Usuń wszystkie wartości większe niż 10.
 Wyświetl set na ekranie.
  */
 
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Random;
+
 public class Rozwiazanie {
     public static void main(String[] args) {
+        HashSet<Integer> set = new HashSet<>();
+        Random random = new Random();
+        for(int i=0;i<20;i++){
+            set.add(random.nextInt(20)+1);
+        }
 
+        Iterator<Integer> iterator = set.iterator();
+        while(iterator.hasNext()){
+            int num = iterator.next();
+            if(num>10) iterator.remove();
+        }
+
+        System.out.println(set);
     }
 }
