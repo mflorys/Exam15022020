00f127776c19ea7593b26b2cdf69ea58819d71f9
Iwona Kulesza <tsubame1990@gmail.com>
2020-02-15 11:36:07 +0100

    task 1, task 2,task3

--- src/pl/qc/ita/java/task3/Rozwiazanie.java
+++ src/pl/qc/ita/java/task3/Rozwiazanie.java
@@ -4,16 +4,29 @@ package pl.qc.ita.java.task3;
 Napisz program, który będzie przyjmował liczby od użytkownika. Gdy użytkownik wprowadzi wyraz suma, program wyświetli sumę dotychczas wprowadzonych liczb.
  */
 
+import java.util.Scanner;
+
 public class Rozwiazanie {
     public static void main(String[] args) {
         System.out.println(sumaWprowadzonychLiczb());
     }
 
-    public static int sumaWprowadzonychLiczb(){
+    public static int sumaWprowadzonychLiczb() {
         int sum = 0;
 
         //Twój kod
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("podaj liczbę lub SUMA");
+            String input = scanner.next().toLowerCase().trim();
+            if (input.equals("suma")) {
+                break;
+            }else {
+                sum = sum + Integer.parseInt(input);
+            }
+        }
 
         return sum;
+
     }
 }
