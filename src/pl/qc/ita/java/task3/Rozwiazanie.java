c8ca1a4d180df770c5127334e07a9797c70ff93e
kapogon <kpogonowski@o2.pl>
2020-02-15 11:13:33 +0100

    zad3 java

--- src/pl/qc/ita/java/task3/Rozwiazanie.java
+++ src/pl/qc/ita/java/task3/Rozwiazanie.java
@@ -4,15 +4,25 @@ package pl.qc.ita.java.task3;
 Napisz program, który będzie przyjmował liczby od użytkownika. Gdy użytkownik wprowadzi wyraz suma, program wyświetli sumę dotychczas wprowadzonych liczb.
  */
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Rozwiazanie {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println(sumaWprowadzonychLiczb());
     }
 
-    public static int sumaWprowadzonychLiczb(){
+    public static int sumaWprowadzonychLiczb() throws IOException {
         int sum = 0;
 
-        //Twój kod
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+            String s = reader.readLine();
+            if (!s.equals("suma")) sum += Integer.parseInt(s);
+            else break;
+        }
 
         return sum;
     }
