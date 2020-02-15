c7a2f56c4ed8540039b3bb25dd7421106506fee1
kapogon <kpogonowski@o2.pl>
2020-02-15 11:26:35 +0100

    zad5 java

--- src/pl/qc/ita/java/task5/Rozwiazanie.java
+++ src/pl/qc/ita/java/task5/Rozwiazanie.java
@@ -7,8 +7,25 @@ Wynik dla 50111:
 Parzyste: 1 Nieparzyste: 4
  */
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Rozwiazanie {
-    public static void main(String[] args) {
-        //Twój kod
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int num = Integer.parseInt(reader.readLine());
+        int even = 0;
+        int odd = 0;
+
+        while (num > 0) {
+            int mod = num % 10;
+            if (mod % 2 == 0) even++;
+            else odd++;
+            num = num / 10;
+        }
+
+        System.out.println("Parzyste: " + even + " Nieparzyste: " + odd);
     }
 }
